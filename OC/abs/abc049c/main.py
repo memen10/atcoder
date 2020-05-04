51dc19373b24649e517397f32b14ec2dd5d63703
memen10 <ii.kit.mori@gmail.com>
2020-05-05 05:37:23 +0900

    abs, add abc049c,abc086c

--- OC/abs/abc049c/main.py
+++ OC/abs/abc049c/main.py
@@ -1,5 +1,16 @@
+import sys
+import re
+sys.setrecursionlimit(10**7)
+input = lambda: sys.stdin.readline().strip()
 
 def main():
-    N = int(input())
+    S = input()
+    words = ["eraser","erase","dreamer","dream"]
+    for w in words:
+        S = re.sub(w,"",S)
+    if S=="":
+        print("YES")
+    else:
+        print("NO")
 
 main()
--- OC/abs/abc086c/main.py
+++ OC/abs/abc086c/main.py
@@ -1,5 +1,22 @@
+import sys
+sys.setrecursionlimit(10**7)
+input = lambda: sys.stdin.readline().strip()
 
 def main():
     N = int(input())
+    tx_list = [list(map(int,input().split())) for _ in range(N)]
+
+    pre_ti, pre_x, pre_y = 0,0,0
+    for tx in tx_list:
+        ti,x,y = tx[0],tx[1],tx[2]
+        tmp_ti,tmp_x,tmp_y = ti-pre_ti,x-pre_x,y-pre_y
+        pre_ti,pre_x,pre_y = ti,x,y
+        for i in range(tmp_ti+1):
+            if ((tmp_ti-i)-i) == (abs(tmp_x)+abs(tmp_y)):
+                break
+        else:
+            print("No")
+            exit()
+    print("Yes")
 
 main()
