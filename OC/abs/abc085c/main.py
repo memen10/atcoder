42b281b1e13044851a9014b13ba014721fe801f7
memen10 <ii.kit.mori@gmail.com>
2020-05-05 04:56:33 +0900

    abs,add abc085c

--- OC/abs/abc085c/main.py
+++ OC/abs/abc085c/main.py
@@ -1,5 +1,18 @@
+import sys
+sys.setrecursionlimit(10**7)
+input = lambda: sys.stdin.readline().strip()
 
 def main():
-    N = int(input())
+    N, Y = map(int,input().split())
+
+    for a in range(N+1):
+        for b in range(N+1):
+            if N-a-b>=0:
+                c = N-a-b
+            else:
+                break
+            if 10000*a+5000*b+1000*c == Y:
+                print(a,b,c);exit()
+    print(-1,-1,-1)
 
 main()
