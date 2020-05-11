ee70bef4702dff3d2328fed64174943b502877f5
memen10 <ii.kit.mori@gmail.com>
2020-05-11 23:48:51 +0900

    add abc160,c

--- ABC/abc160/c/main.py
+++ ABC/abc160/c/main.py
@@ -5,10 +5,13 @@ input = lambda: sys.stdin.readline().strip()
 def main():
     K,N = map(int,input().split())
     A_list = list(map(int,input().split()))
+    A_list = [i-A_list[0] for i in A_list]
     dist_list = []
+
     A_list.append(K)
     for i in range(1,len(A_list)):
-        dist_list.append(A_list[i]-A_list[i-1])
+        d = (A_list[i]-A_list[i-1])
+        dist_list.append(d)
     d_sorted = sorted(dist_list)
     print(int(sum(d_sorted[0:N-1])))
 
