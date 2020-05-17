75844e854111b036b956f2b25dd2fe5ba32ac8f5
memen10 <ii.kit.mori@gmail.com>
2020-05-17 13:53:44 +0900

    add abc159,cd

--- ABC/abc159/c/main.py
+++ ABC/abc159/c/main.py
@@ -1,8 +1,15 @@
+import bisect,collections,copy,heapq,itertools,math,numpy,string
 import sys
 sys.setrecursionlimit(10**7)
-input = lambda: sys.stdin.readline().strip()
+
+def S(): return sys.stdin.readline().rstrip()
+def I(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def LS(): return list(sys.stdin.readline().rstrip().split())
 
 def main():
-    N = int(input())
+    L = I()
+    V = (L/3)**float(3.0)
+    print("{:.12f}".format(V))
 
 main()
