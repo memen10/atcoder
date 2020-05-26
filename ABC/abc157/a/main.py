13c5a0f806a72aa41e86bbc7ce4782af9f467fc2
memen10 <ii.kit.mori@gmail.com>
2020-05-27 04:48:53 +0900

    add abc157,ab

---
+++ ABC/abc157/a/main.py
@@ -0,0 +1,15 @@
+import bisect,collections,copy,heapq,itertools,math,numpy,string
+import sys
+sys.setrecursionlimit(10**7)
+
+def S(): return sys.stdin.readline().rstrip()
+def I(): return int(sys.stdin.readline().rstrip())
+def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
+def LS(): return list(sys.stdin.readline().rstrip().split())
+
+def main():
+    N = I()
+    print(int(N/2) if N%2==0 else int(N/2)+1)
+
+main()
+
