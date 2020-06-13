f8bbc8a21ba682103d8afa017764a53cac80bc3a
memen10 <ii.kit.mori@gmail.com>
2020-06-13 13:02:48 +0900

    add abc157,c

--- ABC/abc157/c/main.py
+++ ABC/abc157/c/main.py
@@ -8,8 +8,24 @@ def LI(): return list(map(int,sys.stdin.readline().rstrip().split()))
 def LS(): return list(sys.stdin.readline().rstrip().split())
 
 def main():
-    N = I()
-    A = [LI() for _ in range(N)]
+    N,M = LI()
+    SC = [LI() for _ in range(M)]
+    for i in range(1000):
+        i = str(i)
+        if len(i)==N:
+            for s,c in SC:
+                if i[s-1] == str(c):
+                    # print(i[s-1], c)
+                    continue
+                else:
+                    break
+            else:
+                print(i)
+                exit(0)
+        else:
+            continue
+    else:
+        print(-1)
 
 main()
 
